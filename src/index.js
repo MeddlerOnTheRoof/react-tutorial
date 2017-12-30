5f84a4833f722929310e507f45f3e69104a1420c
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-12-30 00:33:46 -0500

    Fix broken ending bug

--- src/index.js
+++ src/index.js
@@ -106,13 +106,12 @@ function calculateWinner(squares) {
         [2, 4, 6],
     ];
 
-    lines.forEach(line => {
-        const [a, b, c] = line;
-
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
-    });
+    }
 
     let allSquaresFilled = squares.every(square => {
         return square;
